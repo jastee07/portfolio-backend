af704690a8d2c9409865f38c6d786a8c2e096bc2
Jake Steele <jacob.steele@ortec.com>
2021-09-17 16:43:21 -0400

    Added Post & User Profile

---
+++ blog/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from blog.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Post
+        fields = [
+                'id', 'owner', 'title', 'body',
+                'published', 'published_at', 'created_at',
+                'updated_at'
+            ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from blog.models import Post
+from blog.serializers import PostSerializer
+from user_profile.permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 # Create your views here.
+
+
+class PostViewset(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_class = [IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly]
