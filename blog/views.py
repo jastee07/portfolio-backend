29c93a8b09cf3bfbdef83f6c487a839dbb79826f
Jake Steele <jake.steele54@gmail.com>
2021-09-18 13:12:53 -0400

    Updated Post queryset for reader or editor

--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,10 @@ class PostViewset(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly, IsAuthenticatedOrReadOnly]
+
+
+    def get_queryset(self):
+        if self.request.user.is_staff and self.request.query_params.get('editor') == 'True':
+            return Post.objects.all()
+        else:
+            return Post.objects.filter(published=True)
