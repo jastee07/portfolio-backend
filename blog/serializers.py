6f67cd0a3264caca541fe083f5a924754fe2f5c8
Jake Steele <jacob.steele@ortec.com>
2021-09-28 16:06:47 -0400

    fixed serializer

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,14 +1,22 @@
 from rest_framework import serializers
 from blog.models import Post
+from django.contrib.auth.models import User
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id', 'first_name', 'last_name'
+        ]
 
 
 class PostSerializer(serializers.ModelSerializer):
-    
+    owner = AuthorSerializer(read_only=True)
     class Meta:
         model = Post
         fields = [
                 'id', 'owner', 'title', 'body',
                 'published', 'published_at', 'created_at',
-                'updated_at'
+                'updated_at', 'slug'
             ]
         lookup_field = 'slug'
